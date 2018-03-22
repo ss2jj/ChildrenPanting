898f67349c267d1c7572e51995037a9200e83ea7
ss2jj <xjontheway@gmail.com>
2018-03-22 15:49:42 +0800

    完成简笔画显示画面

--- src/com/xxw/painting/SimplePaintActivity.java
+++ src/com/xxw/painting/SimplePaintActivity.java
@@ -5,8 +5,13 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.xxw.painting.utils.DataDao;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
@@ -14,11 +19,14 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
 import android.widget.GridView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 public class SimplePaintActivity extends Activity {
-	 private GridView gridView_low,gridView_medium,gridView_high;
-	 private List<Map<String, Object>> dataList_low,dataList_medium,dataList_high;
-	 private SimpleAdapter adapter_low,adapter_medium,adapter_high;
+	 private GridView gridView_low;
+	 private List<Map<String, Object>> dataList_low;
+	 private SimpleAdapter adapter_low;
+	 private TextView mTextViewContent;
+	 private StringBuilder mStrContent;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -26,8 +34,15 @@ public class SimplePaintActivity extends Activity {
 		getWindow().setFlags(WindowManager.LayoutParams. FLAG_FULLSCREEN , WindowManager.LayoutParams. FLAG_FULLSCREEN);  
 		setContentView(R.layout.activity_simplepainting);
 		gridView_low = (GridView) findViewById(R.id.simplepainting_gridview_low);
-		gridView_medium = (GridView) findViewById(R.id.simplepainting_gridview_medium);
-		gridView_high = (GridView) findViewById(R.id.simplepainting_gridview_high);
+		mTextViewContent =(TextView) findViewById(R.id.simple_shuoming);
+		mStrContent=new StringBuilder();
+	        //两个tab键，用于段落开头
+        mStrContent.append("    ").append("学习简笔画，可以从画点线开始练习，"
+                ).append("继而过渡到形，即用简练的笔法通过线与线的结合来组合图形。").append("\n")
+        .append("    ").append("现在，点击下方的卡通图案，开始学习绘画吧!");
+        mTextViewContent.setText(mStrContent.toString());
+		Typeface mtypeface=Typeface.createFromAsset(getAssets(),"youyuan.TTF");
+		 mTextViewContent.setTypeface(mtypeface);
 		initData();
 		String[] from={"img","text"};
 
@@ -35,42 +50,34 @@ public class SimplePaintActivity extends Activity {
 
         adapter_low=new SimpleAdapter(this, dataList_low, R.layout.simple_grid_view_item, from, to);
         gridView_low.setAdapter(adapter_low);
-        adapter_medium=new SimpleAdapter(this, dataList_low, R.layout.simple_grid_view_item, from, to);
-        gridView_medium.setAdapter(adapter_medium);
-        adapter_high=new SimpleAdapter(this, dataList_low, R.layout.simple_grid_view_item, from, to);
-        gridView_high.setAdapter(adapter_high);
+       
         gridView_low.setOnItemClickListener(new OnItemClickListener() {
 			
 			@Override
 			public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
 				// TODO Auto-generated method stub
-				
+				Log.d("SimplePaintActivity", "arg2 "+arg2);
 			}
 		});
 	}
 	
 	 void initData() {
-	        //图标
-	        int icno[] = { R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher,
-	                R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher,
-	                R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher,
-	                R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher,
-	                R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher, R.drawable.ic_launcher,
-	                R.drawable.ic_launcher, R.drawable.ic_launcher};
-	        //图标下的文字
-	        String name[]={"时钟","信号","宝箱","秒钟","大象","FF","记事本","书签","印象","商店","主题","迅雷","","","","","","","","","","","",""};
+	      
 	        dataList_low = new ArrayList<Map<String, Object>>();
-	        dataList_medium = new ArrayList<Map<String, Object>>();
-	        dataList_high = new ArrayList<Map<String, Object>>();
-	        for (int i = 0; i <icno.length; i++) {
+	      
+	        for (int i = 0; i <DataDao.icno.length; i++) {
 	            Map<String, Object> map=new HashMap<String, Object>();
-	            map.put("img", icno[i]);
-	            map.put("text",name[i]);
+	            map.put("img", DataDao.icno[i]);
+	            map.put("text",DataDao.name[i]);
 	            dataList_low.add(map);
-	            dataList_medium.add(map);
-	            dataList_high.add(map);
+	           
 	        }
 	    }
 
 	
+	 public void back(View view)	{
+		 
+		  startActivity(new Intent(SimplePaintActivity.this,MainActivity.class));
+		  finish();
+	 }
 }
---
+++ src/com/xxw/painting/utils/DataDao.java
@@ -0,0 +1,13 @@
+package com.xxw.painting.utils;
+
+import com.xxw.painting.R;
+
+public class DataDao {
+	  //图标
+    public static int icno[] = { R.drawable.ballon, R.drawable.cat, R.drawable.crab,
+            R.drawable.frog, R.drawable.rabbit, R.drawable.snail, R.drawable.turkey,
+            R.drawable.zhishengji, R.drawable.zy};
+    //图标下的文字
+    public static String name[]={"热气球","小猫","螃蟹","青蛙","兔子","蜗牛","火鸡","直升机","章鱼"};
+
+}
